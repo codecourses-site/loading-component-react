2774781acba0e920f1cfde4e3bacb63aed02e4da
hieptl <hieptl.developer@gmail.com>
2022-05-09 00:11:33 +0700

    updated README.md

--- src/componnets/Loading.js
+++ src/componnets/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div id="loading" className="loading hidden">
+    <div className="loading">
       <div className="lds-roller">
         <div></div>
         <div></div>
